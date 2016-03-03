fb275cdf0076250bf83266ed2a957a7593fdf7f2
prupro <pruviola@gmail.com>
2016-03-03 18:13:14 +0530

    init to rand float and printing DONE

---
+++ 2/codes/matMultSerial.c
@@ -0,0 +1,57 @@
+#define N 100
+#define NR N
+#define NC N
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printMat(float A[NR][NC]);
+void initMat(float A[NR][NC]);
+
+int main(){
+	
+	float A[NR][NC];
+	initMat(A);		/* fills A with random floats */
+
+	float B[NR][NC];
+	initMat(B);
+
+	float C[NR][NC] = {{0}}; /* initialize to 0 */
+
+	int i,j,k;
+	
+		
+	return 0;
+}
+
+
+void printMat(float A[NR][NC]){
+	
+	int i,j;
+
+	for( i=0; i<NR; i++ ){
+		printf("ROW %d:",i+1);
+		for( j=0; j<NC; j++ ){
+			printf("%.3f\t",A[i][j]);	
+		}
+		printf("\n");
+	}
+
+}
+
+void initMat(float A[NR][NC]){
+	
+	srand( (unsigned int) time(NULL) );
+	
+	float range = 10.0; /* max element in the array */
+
+	int i,j;
+
+	for( i=0; i < NR; i++){
+		for( j=0; j<NC; j++){
+			A[i][j] = ( (float)rand() / (float)(RAND_MAX) ) * range;
+		}
+	} 
+
+}
