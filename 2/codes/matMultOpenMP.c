99a10f6bd953b549abdde6d2949d57166c56bd85
prupro <pruviola@gmail.com>
2016-03-07 17:30:21 +0530

    has wrong timing code; this is not cache friencly

---
+++ 2/codes/matMultOpenMP.c
@@ -0,0 +1,75 @@
+#define N 1000
+#define NR N
+#define NC N
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+void printMat(float A[NR][NC]);
+void initMat(float A[NR][NC]);
+
+int main(){
+	
+	static float A[NR][NC];
+
+	static float B[NR][NC];
+
+	static float C[NR][NC] = {{0}}; /* initialize to 0 */
+	
+	time_t start_time, end_time;
+
+	start_time = time(NULL);
+	int i,j,k;
+	initMat(A);		/* fills A with random floats */
+	initMat(B);
+#pragma omp parallel for private(j,k)
+	for( i=0; i<NR; i++ ){
+		for( j=0; j<NC; j++ ){
+			for( k=0; k<NC; k++){
+				C[i][j] = C[i][j] + A[i][k]*B[k][j];
+			}
+
+		}
+	}	
+	
+	end_time = time(NULL);	
+
+	printMat(C);
+
+	printf("\n Time taken is %f \n",(float)(end_time - start_time));///CLOCKS_PER_SEC);
+
+	return 0;
+}
+
+
+void printMat(float A[NR][NC]){
+
+	int i,j;
+
+	for( i=0; i<NR; i++ ){
+		printf("ROW %d:",i+1);
+		for( j=0; j<NC; j++ ){
+			printf("%.3f\t",A[i][j]);	
+		}
+		printf("\n");
+	}
+
+}
+
+void initMat(float A[NR][NC]){
+
+	srand( (unsigned int) time(NULL) );
+
+	float range = 1.0; /* max element in the array */
+
+	int i,j;
+
+	for( i=0; i < NR; i++){
+		for( j=0; j<NC; j++){
+			A[i][j] = ( (float)rand() / (float)(RAND_MAX) ) * range;
+		}
+	} 
+
+}
